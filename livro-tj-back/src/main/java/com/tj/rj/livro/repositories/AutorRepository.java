37de7ad3c821f2a596be9fc09ae3b0a31e831179
ibimonpm <ibimonpm@hotmail.com>
2023-10-31 23:16:46 -0300

    Divisao de repositorios back e front

--- livro-tj-back/src/main/java/com/tj/rj/livro/repositories/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tj.rj.livro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tj.rj.livro.domain.Autor;
-
-public interface AutorRepository extends JpaRepository<Autor, Long>{
-
-}
